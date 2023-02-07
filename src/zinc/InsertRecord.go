b520d00b59f82bb64971cba3dd4b1ab0a1dadf4c
perqueza72 <practica22correo@gmail.com>
2023-02-07 17:31:52 -0500

    Change Insert record function.

--- src/zinc/InsertRecord.go
+++ src/zinc/InsertRecord.go
@@ -1,24 +1,34 @@
 package zinc_handler
 
 import (
+	"bytes"
 	constants "constants_project"
+	"encoding/json"
 	"io"
 	"log"
 	models "models_zinc"
 	"net/http"
 )
 
-func InsertSingleRecord(index_name string, data models.IRequestData) ([]byte, error) {
-	req, err := http.NewRequest("POST", constants.ZINC_HOST+"/api/"+index_name+"/_doc", data)
+func Model2IRequestData(model interface{}) *models.IRequestData {
+	var buf bytes.Buffer
+
+	json.NewEncoder(&buf).Encode(model)
+	request := models.IRequestData(&buf)
+	return &request
+}
+
+func InsertSingleRecord(data *models.IRequestData) ([]byte, error) {
+	req, err := http.NewRequest("POST", constants.ZINC_HOST+"/api/"+constants.ZINC_EMAIL_INDEX+"/_doc", *data)
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Panic(err)
 		return nil, err
 	}
 	SetHeaders(req)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Panic(err)
 		return nil, err
 	}
 
@@ -26,7 +36,7 @@ func InsertSingleRecord(index_name string, data models.IRequestData) ([]byte, er
 	log.Println(resp.StatusCode)
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Panic(err)
 		return nil, err
 	}
 
